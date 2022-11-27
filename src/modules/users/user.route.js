62e84aabcf5713d1b1380f8604ef771d02585854
buidevkhanh <buiduckhanh.dev@gmail.com>
2022-11-27 09:15:29 +0700

    [Author] Update user validate

--- src/modules/users/user.route.js
+++ src/modules/users/user.route.js
@@ -2,17 +2,17 @@ const express = require('express');
 const { isLogin } = require('../../libs/middlewares/login');
 const { validate } = require('../../libs/middlewares/validation');
 const { userRegister, userLogin, userGetInfo , userUpdateInfo} = require('./user.controller');
-const { userReg, userLog } = require('./user.validation');
+const userValidSchema = require('./user.validation');
 
 const _router = express.Router();
 
-_router.post('/user/register', [validate(userReg), userRegister]);
+_router.post('/user/register', [validate(userValidSchema.userReg), userRegister]);
 
-_router.post('/user/login', [validate(userLog), userLogin]);
+_router.post('/user/login', [validate(userValidSchema.userLog), userLogin]);
 
 _router.get('/user', [isLogin, userGetInfo]);
 
-_router.patch('/user', [isLogin, userUpdateInfo]);
+_router.patch('/user', [isLogin, validate(userValidSchema.userUpdateInfo), userUpdateInfo]);
 
 module.exports = {
     router: _router,
--- src/modules/users/user.validation.js
+++ src/modules/users/user.validation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const userReg = Joi.object({
     fullname: Joi.string().required().pattern(/^[a-zA-Z ]{3,}$/).messages({
-        "string.pattern.base": "fullname must contain at least 2 characters including letters and space"
+        "string.pattern.base": "fullname must contain at least 3 characters including letters and space"
     }),
     email: Joi.string().email().required(),
     password: Joi.string().required().pattern(/^[a-zA-Z0-9\\@\\$\\!\\%\\*\\#\\?\\&]{8,}/).messages({
@@ -15,9 +15,21 @@ const userLog = Joi.object({
     password: Joi.string().required().pattern(/^[a-zA-Z0-9\\@\\$\\!\\%\\*\\#\\?\\&]{8,}/).messages({
         "string.pattern.base": "password must contain at least 8 characters including letters, numbers and special characters"
     })
+});
+
+const userUpdateInfo = Joi.object({
+    fullname: Joi.string().optional().allow(null).pattern(/^[a-zA-Z ]{3,}$/).messages({
+        "string.pattern.base": "fullname must contain at least 3 characters including letters and space"
+    }),
+    email: Joi.string().email().optional().allow(null),
+    password: Joi.string().optional().allow(null).pattern(/^[a-zA-Z0-9\\@\\$\\!\\%\\*\\#\\?\\&]{8,}/).messages({
+        "string.pattern.base": "password must contain at least 8 characters including letters, numbers and special characters"
+    }),
+    avatar: Joi.string().optional().allow(null)
 })
 
 module.exports = {
     userReg,
-    userLog
+    userLog,
+    userUpdateInfo
 }
